f7cc18be20bf113b8bb3830dbb22790c445ad423
gabrijelokorn <gabrijel.okorn@gmail.com>
2022-12-18 20:14:03 +0100

    Some fixes on ui

--- emulator/src/main.cpp
+++ emulator/src/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <string>
-#include <gtkmm.h>
-
-#include "EmulatorApp.h"
-// #include "Machine.h"
+#include <cstring>
+#include <gtk/gtk.h>
 
 using std::cin;
 using std::cout;
@@ -11,41 +9,62 @@ using std::endl;
 
 using namespace std;
 
-class HelloWindow : public Gtk::ApplicationWindow {
-    Gtk::Box *cont;
-    Glib::RefPtr<Gtk::Label> display_label;
-    Glib::RefPtr<Gtk::Button> display_btn;
-    Glib::RefPtr<Gtk::Builder> ui;
-public:
-    HelloWindow()
-    : ui{Gtk::Builder::create_from_file("gui.glade")} {
-        if(ui) {
-            ui->get_widget<Gtk::Box>("cont", cont);
-            display_label = Glib::RefPtr<Gtk::Label>::cast_dynamic(
-                ui->get_object("display_label")
-            );
-            display_btn = Glib::RefPtr<Gtk::Button>::cast_dynamic(
-                ui->get_object("display_button")
-            );
-            if(cont && display_label && display_btn) {
-                display_btn->signal_clicked().connect(
-                [this]() {
-                    display_label->set_text("Hello World");
-                });
-                add(*cont);
-            }
-        }
-        set_title("Simple Gtk::Builder demo");
-        set_default_size(400, 400);
-        show_all();
+static void on_button_step_clicked (GtkWidget* widget, gpointer data) {
+    cout << "clicked\n";
+}
+
+static void menu_response(GtkWidget* menu_item, gpointer data)
+{
+    if(strcmp(gtk_menu_item_get_label(GTK_MENU_ITEM(menu_item)), "New") == 0)
+    {
+        g_print("You pressed New\n");
     }
-};
+}
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
-  auto app = Gtk::Application::create("org.gtkmm.examples.base");
+    gtk_init(&argc, &argv);
 
-  EmulatorApp emulator_app;
+    GtkWidget *window;
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
 
-  return app->run(emulator_app);
+    GtkWidget *menu_bar, *menu_item, *file_menu, *settings_menu, *menu_vbox, *test_button;
+
+    menu_bar = gtk_menu_bar_new();
+    
+    file_menu = gtk_menu_new();
+    settings_menu = gtk_menu_new();
+
+    menu_item = gtk_menu_item_new_with_label("File");
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item), file_menu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu_bar), menu_item);
+
+    menu_item = gtk_menu_item_new_with_label("Settings");
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item), settings_menu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu_bar), menu_item);
+
+    menu_item = gtk_menu_item_new_with_label("New");
+    gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), menu_item);
+    g_signal_connect(menu_item, "activate", G_CALLBACK(menu_response), NULL);
+
+    menu_item = gtk_menu_item_new_with_label("Speed");
+    gtk_menu_shell_append(GTK_MENU_SHELL(settings_menu), menu_item);
+
+    menu_vbox = gtk_vbox_new(0, 0);
+    test_button = gtk_button_new_with_label("hello");
+
+    gtk_box_pack_start(GTK_BOX(menu_vbox), menu_bar, 0, 0, 0);
+    gtk_box_pack_start(GTK_BOX(menu_vbox), test_button, 0, 0, 0);
+
+    gtk_container_add(GTK_CONTAINER(window), menu_vbox);
+
+    gtk_widget_set_size_request(window, 720, 480);
+    gtk_window_set_title(GTK_WINDOW(window), "SicXEmulator");
+    gtk_widget_show_all(window);
+
+    gtk_main();
+
+    return 0;
 }
