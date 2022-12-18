685c38af656e664342cca3a5cb5fb774b175cbb9
gabrijelokorn <gabrijel.okorn@gmail.com>
2022-12-18 02:31:00 +0100

    initial gui commit

--- emulator/src/main.cpp
+++ emulator/src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
+#include <gtkmm.h>
 
-#include "Machine.h"
+// #include "Machine.h"
 
 using std::cin;
 using std::cout;
@@ -9,7 +10,45 @@ using std::endl;
 
 using namespace std;
 
-int main() {
-  Machine SICXEMachine;
-  return 0;
+void on_button_start_clicked (Gtk::Button test) {
+
+}
+
+void on_button_step_clicked (Gtk::Button test) {
+
+}
+
+void on_app_activate()
+{
+  // Load the GtkBuilder file and instantiate its widgets:
+  auto refBuilder = Gtk::Builder::create();
+  try
+  {
+    refBuilder->add_from_file("gui.glade");
+  }
+  catch(const Glib::FileError& ex)
+  {
+    std::cerr << "FileError: " << ex.what() << std::endl;
+    return;
+  }
+  catch(const Glib::MarkupError& ex)
+  {
+    std::cerr << "MarkupError: " << ex.what() << std::endl;
+    return;
+  }
+  catch(const Gtk::BuilderError& ex)
+  {
+    std::cerr << "BuilderError: " << ex.what() << std::endl;
+    return;
+  }
+}
+
+int main(int argc, char** argv) {
+
+  auto app = Gtk::Application::create("org.gtkmm.examples.base");
+
+  app->signal_activate().connect([] () { on_app_activate(); });
+
+  return app->run(argc, argv);
+  // return app->make_window_and_run<MainWin>(argc, argv);
 }
